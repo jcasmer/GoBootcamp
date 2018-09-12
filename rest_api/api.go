b3b58f036abab3afdfc0b6290dc5c878a0d05fbb
Juan <juan.castano@globant.com>
2018-09-12 15:53:20 -0500

    added addArticles method, added some validations

--- rest_api/api.go
+++ rest_api/api.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"math/rand"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/jcasmer/GoBootcamp/memory_db/db"
@@ -14,20 +14,23 @@ import (
 
 var (
 	//ErrNotFound es retornado cuando no existe el indice o el item en la bd
-	ErrRequired = errors.New("is required")
+	ErrRequired          = errors.New("is required")
+	ErrOwnerRequired     = errors.New("Owner is required")
+	ErrIdArticleRequired = errors.New("IdArticle is required")
+	ErrQuantity          = errors.New("Quantity must be greater than 0")
 )
 
 // Articles struct (Model)
 type Articles struct {
-	idArticle string `json:"id_article"`
-	Quantity  string `json:"quantity"`
+	Id       string `json:"id"`
+	Quantity int    `json:"quantity"`
 }
 
 // Car struct
 type Carts struct {
-	Id      string      `json:"id"`
-	Owner   string      `json:"owner"`
-	Article []*Articles `json:"articles"`
+	Id      string     `json:"id"`
+	Owner   string     `json:"owner"`
+	Article []Articles `json:"articles"`
 }
 
 type CartsArticles struct {
@@ -49,6 +52,38 @@ func NewService(db db.DbInter) *Service {
 	return &s
 }
 
+func (c Carts) ValidateCart() error {
+	//cart validations
+	if c.Owner == "" {
+		return ErrOwnerRequired
+	}
+	_, err := strconv.Atoi(c.Owner)
+	if err == nil {
+		return errors.New("Owner must be string")
+	}
+	return nil
+}
+
+func (a Articles) ValidateArticle() error {
+	//article validations
+	fmt.Println(a)
+	if a.Id == "" {
+		return ErrIdArticleRequired
+	}
+	_, err := strconv.Atoi(a.Id)
+	if err != nil {
+		return errors.New("id article must be int")
+	}
+	if a.Quantity <= 0 {
+		return ErrQuantity
+	}
+	// _, err = strconv.Atoi(a.Quantity)
+	// if err != nil {
+	// 	return errors.New("quantity must be int")
+	// }
+	return nil
+}
+
 // Get all carts
 func (s *Service) getCarts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -68,12 +103,9 @@ func (s *Service) createCart(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	// if strings.TrimSpace(cart.Id) == "" {
-	// 	http.Error(w, "id is required", http.StatusBadRequest)
-	// 	return
-	// }
-	if strings.TrimSpace(cart.Owner) == "" {
-		http.Error(w, "owner is required", http.StatusBadRequest)
+	err = cart.ValidateCart()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -120,65 +152,57 @@ func (s *Service) getCart(w http.ResponseWriter, r *http.Request) {
 
 }
 
-/*
-// func addArticles(w http.ResponseWriter, r *http.Request) {
-// 	// add article to specific cart
-// 	w.Header().Set("Content-Type", "application/json")
-// 	params := mux.Vars(r)
+func (s *Service) addArticles(w http.ResponseWriter, r *http.Request) {
+	// add article to specific cart
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
 
-// 	if params["id"] == "" {
-// 		http.Error(w, "id cart is required", http.StatusBadRequest)
-// 		return
-// 	}
+	if params["id"] == "" {
+		http.Error(w, "id cart is required", http.StatusBadRequest)
+		return
+	}
 
-// 	var article Articles|
-// 	error := json.NewDecoder(r.Body).Decode(&article)
-// 	if error != nil {
-// 		http.Error(w, error.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-// 	if strings.TrimSpace(article.idArticle) == "" {
-// 		http.Error(w, "id article is required", http.StatusBadRequest)
-// 		return
-// 	}
-// 	if strings.TrimSpace(article.Quantity) == "" {
-// 		http.Error(w, "quantity is required", http.StatusBadRequest)
-// 		return
-// 	}
+	var article Articles
+	err := json.NewDecoder(r.Body).Decode(&article)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err = article.ValidateArticle()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// value, _ := json.Marshal(cart)
 
-// 	// value, _ := json.Marshal(cart)
+	var cart Carts
+	car, erro := s.dataBase.Retrieve(params["id"])
+	if erro != nil {
+		http.Error(w, erro.Error(), http.StatusNotFound)
+		return
+	}
 
-// 	d, erro := db.OpenDB(dbName)
-// 	if erro != nil {
-// 		http.Error(w, erro.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-// 	var cart Carts
-// 	car, erro := d.Retrieve(params["id"])
-// 	if erro != nil {
-// 		http.Error(w, erro.Error(), http.StatusNotFound)
-// 		return
-// 	}
+	_ = json.Unmarshal([]byte(car), &cart)
+	// validate the article to add not exists
+	for _, item := range cart.Article {
+		if item.Id == article.Id {
+			http.Error(w, "The article to add already exist", http.StatusNotFound)
+			return
+		}
+	}
 
-// 	_ = json.Unmarshal([]byte(car), &cart)
+	cart.Article = append(cart.Article, article)
 
-// 	cart.Article = append(cart.Article, article)
+	value, _ := json.Marshal(cart)
 
-// 	// resul := d.CreateWithIndex(cart.Id, string(value))
-// 	// if resul != nil {
-// 	// 	http.Error(w, resul.Error(), http.StatusBadRequest)
-// 	// 	return
-// 	// }
+	resul := s.dataBase.Update(cart.Id, string(value))
+	if resul != nil {
+		http.Error(w, resul.Error(), http.StatusBadRequest)
+		return
+	}
 
-// 	er := d.Close(dbName)
-// 	if er != nil {
-// 		http.Error(w, er.Error(), http.StatusBadRequest)
-// 		return
-// 	}
+	w.WriteHeader(http.StatusNoContent)
+	// json.NewEncoder(w).Encode(cart)
+	// http. .StatusCreated
 
-// 	w.WriteHeader(http.StatusCreated)
-// 	json.NewEncoder(w).Encode(cart)
-// 	// http. .StatusCreated
-
-// }
-*/
+}
